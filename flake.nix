12ee299f1bcec76cbbd7293a576c7421f5dc68e3
Sam Stites <stites@users.noreply.github.com>
2023-02-03 16:52:37 -0500

    workspace

--- flake.nix
+++ flake.nix
@@ -2,35 +2,56 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     devenv.url = "github:cachix/devenv";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, devenv, ... } @ inputs:
-    let
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
-    in
-    {
-      devShells = forAllSystems
-        (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-            };
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = with pkgs; [ hello tree-sitter ];
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        craneLib = inputs.crane.lib.${system};
+        my-crate = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          buildInputs = [
+            # Add additional build inputs here
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+        };
+      in
+      {
+        checks = {
+          inherit my-crate;
+        };
 
-                  enterShell = ''
-                    hello
-                  '';
-                }
-              ];
-            };
-          });
-    };
+        packages.default = my-crate;
+
+        apps.default = inputs.flake-utils.lib.mkApp {
+          drv = my-crate;
+        };
+
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              # https://devenv.sh/reference/options/
+              packages = with pkgs; [
+                hello tree-sitter
+                cargo
+                rustc
+              ] # ++ builtins.attrValues self.checks
+              ;
+
+              enterShell = ''
+                hello
+              '';
+            }
+          ];
+        };
+      });
 }
