1e00fcfdf1abcc36a7f592c1c69b36abca01290e
Sam Stites <stites@users.noreply.github.com>
2023-02-19 16:45:49 -0500

    render substitutions

--- core/src/render.rs
+++ core/src/render.rs
@@ -1,6 +1,9 @@
+use crate::grammar::*;
+use crate::semantics::UniqueId;
 use itertools::*;
 /// helper functions for rendering
 use rsdd::repr::bdd::*;
+use std::collections::HashMap;
 
 pub fn rendervec(fs: &Vec<String>) -> String {
     format!("[{}]", fs.join(", "))
@@ -12,6 +15,16 @@ pub fn renderfloats(fs: &Vec<f64>, high_prec: bool) -> String {
 
 pub fn renderbdds(fs: &Vec<BddPtr>) -> String {
     rendervec(&fs.iter().map(|b| b.print_bdd()).collect_vec())
+}
+
+pub fn rendersubs(fs: &HashMap<UniqueId, Vec<BddPtr>>) -> String {
+    format!(
+        "[{}]",
+        &fs.iter()
+            .map(|(k, b)| format!("{:?}: {}", k, renderbdds(b)))
+            .collect_vec()
+            .join(" ,")
+    )
 }
 pub fn fmt_f64(high_precision: bool) -> impl Fn(f64) -> String {
     if high_precision {
