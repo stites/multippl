4e7f29a0d35dc92dac5c92c6cde141ea4d652cbb
Sam Stites <stites@users.noreply.github.com>
2023-03-21 00:57:15 -0400

    start bash runner

---
+++ core/plots/grids.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+for determinism in 0.75 0.25 0.5 0.0; do
+    for comptype in "approx" "exact"; do
+        for gridsize in 3 6 9 12 15; do
+            CSV="${comptype}-g${gridsize}-d${determinism}.csv"
+            if cargo run --bin plot-grids --features=plots -- --gridsize $gridsize --comptype $comptype --determinism $determinism --csv "${CSV}"; then
+                noti -o -m "${CSV} done"
+            else
+                noti -o -m "${CSV} restarting..."
+                for i in 1 2 3 4 5; do
+                    if cargo run --bin plot-grids --features=plots -- --gridsize $gridsize --comptype $comptype --determinism $determinism --csv "${CSV}" --runs 1; then
+                        noti -o -m "${CSV} restart#${i} done"
+                    else
+                        noti -o -m "${CSV} restart#${i} failed!"
+                    fi
+                done
+            fi
+        done
+    done
+done
