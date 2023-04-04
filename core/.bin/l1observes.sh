03da13c9d9a41f3b26ff99d6c9304fdc81fae95c
Sam Stites <stites@users.noreply.github.com>
2023-04-04 16:28:17 -0400

    l1 observes runner

---
+++ core/.bin/l1observes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+for gridsize in 3 4; do
+    for determinism in 0.0; do
+        if cargo run --release --bin plot-grids --features=plots -- \
+              -v 1 \
+              --path "out/obs/$gridsize-$determinism/" \
+              variance \
+              --sliding-obs \
+              --overwrite-csv \
+              --gridsize $gridsize \
+              --comptype approx \
+              --determinism $determinism \
+              --steps 10000 \
+              --runs 3; then
+            noti -o -m "${gridsize} ${determinism} done"
+         fi
+            noti -o -m "${gridsize} ${determinism} failed"
+    done
+done
