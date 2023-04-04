28709c608a5443a3aea76c74de2c72349999fae0
Sam Stites <stites@users.noreply.github.com>
2023-04-03 22:47:44 -0400

    add noti

--- core/.bin/l1.sh
+++ core/.bin/l1.sh
@@ -2,12 +2,15 @@
 
 for gridsize in 3 4 5 6; do
     for determinism in 0.0; do
-        cargo run --release --bin plot-grids --features=plots -- \
+        if cargo run --release --bin plot-grids --features=plots -- \
               -v 1 \
               variance \
               --gridsize $gridsize \
               --comptype approx \
               --determinism $determinism \
-              --runs 1000000
+              --runs 1000000; then
+            noti -o -m "${gridsize} ${determinism} done"
+         fi
+            noti -o -m "${gridsize} ${determinism} failed"
     done
 done
