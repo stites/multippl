46f82e5b222de9f2a61d924a28e82e13a1799432
Sam Stites <stites@users.noreply.github.com>
2023-04-04 20:05:33 -0400

    add to runnner

--- core/.bin/l1observes.sh
+++ core/.bin/l1observes.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 for gridsize in 3 4; do
-    for determinism in 0.0; do
-        if cargo run --release --bin plot-grids --features=plots -- \
-              -v 1 \
-              --path "out/obs/$gridsize-$determinism/" \
-              variance \
-              --sliding-obs \
-              --overwrite-csv \
-              --gridsize $gridsize \
-              --comptype approx \
-              --determinism $determinism \
-              --steps 1000000 \
-              --runs 3; then
-            noti -o -m "${gridsize} ${determinism} done"
-         fi
-            noti -o -m "${gridsize} ${determinism} failed"
+    for clausesize in 2 3 4; do
+        for determinism in 0.0; do
+            if cargo run --release --bin plot-grids --features=plots -- \
+                  -v 1 \
+                  --path "out/obs/$gridsize-$determinism/" \
+                  variance \
+                  --sliding-obs $clausesize \
+                  --overwrite-csv \
+                  --gridsize $gridsize \
+                  --comptype approx \
+                  --determinism $determinism \
+                  --steps 1000000 \
+                  --runs 3; then
+                noti -o -m "${gridsize} ${determinism} done"
+             fi
+                noti -o -m "${gridsize} ${determinism} failed"
+        done
     done
 done
