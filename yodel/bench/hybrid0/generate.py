13fa7d6d0d913068aa9d490d0bfa7a1cb8316429
Sam Stites <stites@users.noreply.github.com>
2023-11-24 17:17:31 -0500

    hybrid0 ready

---
+++ yodel/bench/hybrid0/generate.py
@@ -0,0 +1,43 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from util import as_json
+
+import random
+import numpy as np
+from numpy.random import normal
+
+random.seed(3)
+np.random.seed(3)
+
+count = 1000
+
+def flips(p, l):
+    return np.array([random.random() < p for _ in range(l)])
+
+def conditional_flips(ps, cond_on):
+    (pt, pf) = ps
+    t_ix = np.where(cond_on == True)
+    t = flips(pt, t_ix[0].shape[0])
+    f_ix = np.where(cond_on == False)
+    f = flips(pf, f_ix[0].shape[0])
+    nxt = np.empty((cond_on.shape[0],), dtype=bool)
+    nxt[t_ix] = t
+    nxt[f_ix] = f
+    return nxt
+
+a = flips(0.5, count)
+b = conditional_flips((1./4, 2./3), a)
+c = normal(loc=2, scale=0.3, size=count)
+
+def noise(scale, size=count):
+    return normal(loc=0.0, scale=scale, size=size)
+
+d = np.empty((count,))
+d[ a &  b] = normal(loc=2, scale=0.3, size=c[ a &  b].shape[0])
+d[ a & ~b] = normal(loc=3, scale=0.3, size=c[ a & ~b].shape[0])
+d[~a &  b] = normal(loc=4, scale=0.3, size=c[~a &  b].shape[0])
+d[~a & ~b] = normal(loc=5, scale=0.3, size=c[~a & ~b].shape[0])
+
+if __name__ == "__main__":
+    keys = as_json(a, b, c, d, batch_size=5, outname="data", outext="json", col_names=["a", "b", "c", "d"])
+    print(sorted(list(keys)))
