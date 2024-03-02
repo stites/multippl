4f67ead593f24b4e999583f0eb1b3a2856b9bd3b
Sam Stites <stites@users.noreply.github.com>
2024-03-02 18:40:33 -0500

    unify mgr creation and declaration

--- yodel/src/data/mod.rs
+++ yodel/src/data/mod.rs
@@ -26,7 +26,7 @@ pub use rustc_hash::{FxHashMap, FxHashSet};
 
 // pub type Mgr = BddManager<AllTable<BddPtr>>;
 pub type Mgr = BddManager<BddApplyTable<BddPtr>>;
-pub fn new_manager(maxlbl : u64) -> Mgr {
+pub fn new_manager(maxlbl: u64) -> Mgr {
     // Mgr::new_default_order(maxlbl as usize)
     // Mgr::new_default_order(0_usize);
     Mgr::new_default_order_lru(0_usize)
