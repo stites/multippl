d55e3ca510e17c139c538742910b32e580cf1ec0
Sam Stites <stites@users.noreply.github.com>
2024-03-04 17:25:24 -0500

    arrival

--- yodel/bench/arrival/3x3/main.py
+++ yodel/bench/arrival/3x3/main.py
@@ -18,6 +18,11 @@ def probfn(i, j):
        case (2, 2): return [3.0 / 7.0, 3.0 / 8.0, 8.0 / 9.0, 9.0 / 11.0]
        case _: raise Exception
 
+sites = ["npackets"]
+truth = [2.3127377834371137]
+
+model = lambda: mkarrival(3, probfn)
+
 # ising_model = lambda: mkgrid(3, probfn)
 
 # def model():
@@ -38,39 +43,69 @@ def probfn(i, j):
 # # Importance sampling
 #importance = Importance(model).run()
 #marginal = EmpiricalMarginal(importance, sites=["npackets"])
+
+# if __name__ == "__main__":
+#     num_samples = 1_000
+#     num_runs = 2
+#     start_seed = 0
+#     args = {}
+
+    # l1s = []
+    # times = []
+    # for run, seedoffset in enumerate(range(1,num_runs+1)):
+    #     seed = start_seed + seedoffset
+    #     print(f"starting run {run+1}/{num_runs} (with seed: {seed})", flush=True)
+    #     torch.manual_seed(seed)
+    #     np.random.seed(seed)
+    #     random.seed(seed)
+    #     start = time.time()
+
+    #     importance = Importance(model, num_samples=num_samples)
+    #     posterior = importance.run(*args)
+
+    #     ms = allmarg(posterior, sites, num_samples=num_samples) # don't think need a full
+    #     print("am")
+    #     end = time.time()
+    #     times.append(end - start)
+    #     print(times[-1], "s", flush=True)
+    #     l1 = compute_l1(ms, truth)
+    #     printit(sites, ms, l1)
+    #     l1s.append(sum(l1))
+    # #return (l1s, times)
+
 if __name__ == "__main__":
-    num_samples = 1_000
-    num_runs = 2
-    start_seed = 0
-    args = {}
-    sites = ["npackets"]
-    truth = [2.3127377834371137]
+    import argparse
 
-    model = lambda: mkarrival(3, probfn)
+    parser = argparse.ArgumentParser(description="generate data for simple HMMs")
+    parser.add_argument("--num-samples", default=10_000, type=int,)
+    parser.add_argument("--num-runs", default=10, type=int,)
+    parser.add_argument("--seed", default=0, type=int,)
+    args = parser.parse_args()
 
-    l1s = []
-    times = []
-    for run, seedoffset in enumerate(range(1,num_runs+1)):
-        seed = start_seed + seedoffset
-        print(f"starting run {run+1}/{num_runs} (with seed: {seed})", flush=True)
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-        random.seed(seed)
+    if args.num_runs > 1:
+        (l1s, times) = runall(model, sites, truth, num_runs=args.num_runs, num_samples=args.num_samples, start_seed=args.seed)
+        print("--------")
+        runs = len(l1s)
+        print(f"averages over {runs} runs:")
+        print("wallclock:", sum(times) / len(times), "s")
+        print("       L1:", sum(l1s) / len(l1s))
+    else:
+        # we are benchmarking, expect the same output as yodel
+        torch.manual_seed(args.seed)
+        np.random.seed(args.seed)
+        random.seed(args.seed)
         start = time.time()
-
-        importance = Importance(model, num_samples=num_samples)
-        posterior = importance.run(*args)
-
-        ms = allmarg(posterior, sites, num_samples=num_samples) # don't think need a full
-        print("am")
+        importance = Importance(model, num_samples=args.num_samples)
+        posterior = importance.run()
+        xs = allmarg(posterior, sites, num_samples=args.num_samples)
         end = time.time()
-        times.append(end - start)
-        print(times[-1], "s", flush=True)
-        l1 = compute_l1(ms, truth)
-        printit(sites, ms, l1)
-        l1s.append(sum(l1))
-    #return (l1s, times)
-
+        s = end - start
+        print(" ".join([f"{x}" for x in xs]))
+        print("{:.3f}ms".format(s * 1000))
+        # if s < 1.0:
+        #     print("{:.3f}ms".format(s / 1000))
+        # else:
+        #     print("{:.3f}s".format(s))
 
 
 # # Estimate the expectation
