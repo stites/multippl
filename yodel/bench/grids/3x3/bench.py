8f801febb6b8271fef9c5c85d514553f735226a3
Sam Stites <stites@users.noreply.github.com>
2024-03-10 14:00:11 -0400

    handle timeouts

--- yodel/bench/grids/3x3/bench.py
+++ yodel/bench/grids/3x3/bench.py
@@ -6,117 +6,111 @@ import sys
 from tqdm import tqdm
 import subprocess
 import time
+from multiprocessing.dummy import Pool as ThreadPool
 from multiprocessing import Pool
-from subprocess import STDOUT, check_output, CalledProcessError, TimeoutExpired
-import signal
+import multiprocessing.context as ctx
 
-USE_NOTI=False
-TIMEOUT_SEC = 5
-#timeout_sec = 60 * 5 # 5m
+USE_NOTI=True
+TIMEOUT_SEC=60 * 5 # 5min
 repo_dir = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
 benchdir = f"{repo_dir}/yodel/bench/"
 
-import threading
+class Abortable:
+    def __init__(self, func, timeout):
+        self.timeout = timeout
+        self.func = func
 
-class Proc(threading.Thread):
-    def __init__(self, run, mainfile, nsteps, nruns, initial_seed, cmd, logdir, with_seed, needs_timer):
-        threading.Thread.__init__(self)
-        self.date = time.strftime("%Y-%m-%d", time.localtime())
-        self.hm  = time.strftime("%H:%M", time.localtime())
-        self.start_time = time.time()
-        self.seed = run + initial_seed
-        self.cmd = cmd if not with_seed else cmd + [str(self.seed)]
-        self.mainfile = mainfile
-        self.nsteps = nsteps
-        self.nruns = nruns
-        self.logdir = logdir
-        self.needs_timer = needs_timer
-        self.outfilepath = logdir + "_".join([
-            f"{self.mainfile.replace('.', '-')}",
-            f"n{self.nsteps}",
-            f"s{self.seed}",
-            f"{self.date}_{self.hm}.log"])
+    def __call__(self, args):
+        run, mainfile, nsteps, nruns, iseed, cmd, logdir, with_seed, needs_timer = args
+        seed = run + iseed
+        start = time.time()
+        outfilepath = mkoutpath(logdir, mainfile, nsteps, seed, date, hm)
 
-    def run(self):
-        self.p = subprocess.Popen(self.cmd) # timeout=TIMEOUT_SEC,
-        self.p.wait()
-        # print("running")
-        with open(self.outfilepath, "w") as outfile:
-            self.p = subprocess.Popen(self.cmd, stdout=outfile) # timeout=TIMEOUT_SEC,
-            if self.needs_timer:
-                outfile.write(f"{self.timeit()}ms\n")
-            self.p.wait()
+        p = ThreadPool(1)
+        res = p.apply_async(self.func, args=args)
+        try:
+            out = res.get(self.timeout)  # Wait timeout seconds for func to complete.
+            return out
+        except (TimeoutError, ctx.TimeoutError):
+            with open(outfilepath, "w") as outfile:
+                outfile.write(f"timeout@{TIMEOUT_SEC} (seconds)\n")
+            noti_failed(mainfile, 124, run, nruns)
+        finally:
+            p.close()
+            p.join()
 
-    def timeit(self):
-        return time.time() - self.start_time
-
-    def run_with_timeout(self, timeout):
-        self.start()
-        self.join(timeout)
-        if self.is_alive():
-            print("terminating")
-            #self.p.terminate()      #use self.p.kill() if process needs a kill -9
-            self.p.kill()      #use self.p.kill() if process needs a kill -9
-            with open(self.outfilepath, "w") as outfile:
-                outfile.write(f"timeout@{timeout} (seconds)\n")
-            self.join()
-        else:
-            if self.run == 0:
-                Proc.noti_success(self.mainfile, 1, self.nruns, self.timeit())
-            if self.p.returncode > 0:
-                with open(self.outfilepath, "w") as outfile:
-                    outfile.write(f"error@{self.timeit()} (milliseconds)\n")
-                Proc.noti_failed(self.mainfile, self.p.returncode, self.run, self.nruns)
-
-    @staticmethod
-    def _noti(title, message):
-        if USE_NOTI:
-            import subprocess, os
-            env = os.environ.copy()
-            subprocess.run(["noti", "-o", "-t", f"\"{title}\"", '-m', f'"{message}"'], env=env)
-        else:
-            print(title, ":", message)
-
-    @staticmethod
-    def noti_failed(mainfile, exitcode, run_ix, num_runs, timeout=0.0):
-        title = f"{mainfile} ({run_ix} / {num_runs}): {exitcode}"
-        if timeout > 0:
-            message = f"timeout after {timeout}s"
-        else:
-            message = "failed!"
-        Proc._noti(title, message)
-
-    @staticmethod
-    def noti_success(mainfile, run_ix, num_runs, sec_per_run):
-        title = f"{mainfile} ({run_ix} / {num_runs})"
-        message = "done! @ {:.2f}s".format(sec_per_run)
-        Proc._noti(title, message)
+def mkoutpath(logdir, mainfile, nsteps, seed, date, hm):
+    outfilepath = logdir + "_".join([
+        f"{mainfile.replace('.', '-')}",
+        f"n{nsteps}",
+        f"s{seed}",
+        f"{date}_{hm}.log"])
+    return outfilepath
 
 
-def runner_(mainfile, cmd, with_seed=True, logdir="logs/", **args):
-    nsteps = args['num_steps']
-    nruns = args['num_runs']
+def proc(run, mainfile, nsteps, nruns, initial_seed, cmd, logdir, with_seed, needs_timer):
+    date = time.strftime("%Y-%m-%d", time.localtime())
+    hm  = time.strftime("%H:%M", time.localtime())
+    seed = run + initial_seed
+    outfilepath = mkoutpath(logdir, mainfile, nsteps, seed, date, hm)
 
     start = time.time()
-    all_args = [(run, mainfile, nsteps, nruns, args['initial_seed'], cmd, logdir, with_seed, args['needs_timer'],) for run in range(nruns)]
-    pbar = tqdm([Proc(*args).run_with_timeout(TIMEOUT_SEC) for args in all_args], total=nruns)
+    cmd = cmd if not with_seed else cmd + [str(seed)]
+    with open(outfilepath, "w") as outfile:
+        p = subprocess.run(cmd, stdout=outfile, timeout=TIMEOUT_SEC)
+        if needs_timer:
+            outfile.write(f"{time.time() - start}ms\n")
 
-    # with Pool(nruns) as p:
-    #     pbar = tqdm(p.imap_unordered(Proc, all_args), total=nruns)
-    #     pbar.set_description(mainfile + f"(n:{nsteps})")
-    #     list(pbar)
+    if run == 0 and p.returncode == 0:
+        end1 = time.time()
+        noti_success(mainfile, 1, nruns, (end1 - start))
+
+    if p.returncode > 0:
+        noti_failed(mainfile, p.returncode, run, nruns)
+
+def runner_(mainfile, cmd, with_seed=True, logdir="logs/", needs_timer=False, **kwargs):
+    nsteps = kwargs['num_steps']
+    nruns = kwargs['num_runs']
+    iseed = kwargs['initial_seed']
+
+    start = time.time()
+    all_args = [(run, mainfile, nsteps, nruns, iseed, cmd, logdir, with_seed, needs_timer) for run in range(nruns)]
+    with Pool(nruns) as p:
+        aworker = Abortable(proc, timeout=5)
+        pbar = tqdm(p.imap_unordered(aworker, all_args), total=nruns)
+        pbar.set_description(mainfile + f"(n:{nsteps})")
+        list(pbar)
     end = time.time()
-    Proc.noti_success(mainfile, nruns, nruns,  (end - start) / nruns)
+    noti_success(mainfile, nruns, nruns,  (end - start) / nruns)
 
-def pyrunner(mainfile, logdir="logs/", **args):
-    cmd = ["python", mainfile, "--num-samples", str(args['num_steps']), "--num-runs", "1", "--seed"]
-    runner_(mainfile, cmd, with_seed=True, logdir=logdir, needs_timer=False, **args)
+def _noti(title, message):
+    if USE_NOTI:
+        import subprocess, os
+        env = os.environ.copy()
+        subprocess.run(["noti", "-o", "-t", f"\"{title}\"", '-m', f'"{message}"'], env=env)
+    else:
+        print(title, ":", message)
 
-def timedrunner(bin, mainfile, logdir="logs/", **args):
+def noti_failed(mainfile, exitcode, run_ix, num_runs):
+    title = f"{mainfile} ({run_ix} / {num_runs}): {exitcode}"
+    message = "failed!"
+    _noti(title, message)
+
+def noti_success(mainfile, run_ix, num_runs, sec_per_run):
+    title = f"{mainfile} ({run_ix} / {num_runs})"
+    message = "done! @ {:.2f}s".format(sec_per_run)
+    _noti(title, message)
+
+def pyrunner(mainfile, logdir="logs/", **kwargs):
+    cmd = ["python", mainfile, "--num-samples", str(kwargs['num_steps']), "--num-runs", "1", "--seed"]
+    runner_(mainfile, cmd, with_seed=True, logdir=logdir, needs_timer=False, **kwargs)
+
+def timedrunner(bin, mainfile, logdir="logs/", **kwargs):
+    # cmd = [benchdir + "time.sh", bin, mainfile]
     cmd = [bin, mainfile]
-    runner_(mainfile, cmd, with_seed=False, logdir=logdir, needs_timer=True, **args)
+    runner_(mainfile, cmd, with_seed=False, logdir=logdir, needs_timer=True, **kwargs)
 
-def yorunner(mainfile, logdir="logs/", **args):
+def yorunner(mainfile, logdir="logs/", **kwargs):
     subprocess.run(["cargo", "build", "--release", "--bin", "yodel"],
                    stdout=subprocess.DEVNULL,
                    stderr=subprocess.DEVNULL,
@@ -128,9 +122,9 @@ def yorunner(mainfile, logdir="logs/", **args):
         yodelcmd = [yodelbin]
         filearg = ["--file", mainfile]
         dataarg = ["--data", "data.json"] if os.path.isfile("data.json") else []
-        nsteps = ["--steps", str(args['num_steps'])]
+        nsteps = ["--steps", str(kwargs['num_steps'])]
         cmd = yodelcmd + filearg + dataarg + nsteps + [ "--rng" ]
-        runner_(mainfile, cmd, with_seed=True, logdir=logdir, needs_timer=False, **args)
+        runner_(mainfile, cmd, with_seed=True, logdir=logdir, needs_timer=False, **kwargs)
 
 if __name__ == "__main__":
     import sys
