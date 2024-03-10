5f16968f6788a481516dec4e65636c7ecd4debc1
Sam Stites <stites@users.noreply.github.com>
2024-03-10 15:14:13 -0400

    observed versions of all grids

---
+++ yodel/bench/grids/3x3/grids3x3-obs01.py
@@ -0,0 +1,71 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils import *
+from yodel import sites3, truth3
+
+def ising_model():
+    x00 = flip("00", 1.0 / 2.0)
+    x01 = edge("01", x00, 1.0 / 3.0, 1.0 / 4.0)
+    x02 = edge("02", x01, 1.0 / 3.0, 1.0 / 4.0)
+    x10 = edge("10", x00, 1.0 / 5.0, 1.0 / 6.0)
+    x20 = edge("20", x10, 1.0 / 5.0, 1.0 / 6.0)
+
+    x11 = node("11", x10, x01, 1.0 / 7.0, 1.0 / 8.0, 1.0 / 9.0, 1.0 / 11.0)
+    x21 = node("21", x20, x11, 2.0 / 7.0, 2.0 / 8.0, 2.0 / 9.0, 2.0 / 11.0)
+    x12 = node("12", x11, x02, 6.0 / 7.0, 6.0 / 8.0, 6.0 / 9.0, 6.0 / 11.0)
+    x22 = node("22", x21, x12, 3.0 / 7.0, 3.0 / 8.0, 8.0 / 9.0, 9.0 / 11.0)
+    return [[x00, x01, x02],
+            [x10, x11, x12],
+            [x20, x12, x22]]
+
+def probfn(i, j):
+   match (i, j):
+       case (0, 0): return [0.5]
+       case (0, _): return [1./3, 1./4]
+       case (_, 0): return [1./5, 1./6]
+       case (1, 1): return [1.0 / 7.0, 1.0 / 8.0, 1.0 / 9.0, 1.0 / 11.0]
+       case (2, 1): return [2.0 / 7.0, 2.0 / 8.0, 2.0 / 9.0, 2.0 / 11.0]
+       case (1, 2): return [6.0 / 7.0, 6.0 / 8.0, 6.0 / 9.0, 6.0 / 11.0]
+       case (2, 2): return [3.0 / 7.0, 3.0 / 8.0, 8.0 / 9.0, 9.0 / 11.0]
+       case _: raise Exception
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="generate data for simple HMMs")
+    parser.add_argument("--num-samples", default=1_000, type=int,)
+    parser.add_argument("--num-runs", default=1, type=int,)
+    parser.add_argument("--seed", default=0, type=int,)
+    args = parser.parse_args()
+
+    model = lambda: mkgrid(3, probfn)
+    model = pyro.condition(model, data={"flip01": torch.tensor(0.0)})
+
+    if args.num_runs > 1:
+        (l1s, times) = runall(model, sites3, truth3, num_runs=args.num_runs, num_samples=args.num_samples, start_seed=args.seed)
+        print("--------")
+        runs = len(l1s)
+        print(f"averages over {runs} runs:")
+        print("wallclock:", sum(times) / len(times), "s")
+        print("       L1:", sum(l1s) / len(l1s))
+        # averages over 10 runs:
+        # wallclock: 42.63511703014374 s
+        #        L1: 0.41128323336703865
+    else:
+        # we are benchmarking, expect the same output as yodel
+        torch.manual_seed(args.seed)
+        np.random.seed(args.seed)
+        random.seed(args.seed)
+        start = time.time()
+        importance = Importance(model, num_samples=args.num_samples)
+        posterior = importance.run()
+        xs = allmarg(posterior, sites3, num_samples=args.num_samples)
+        end = time.time()
+        s = end - start
+        print(" ".join([f"{x}" for x in xs]))
+        print("{:.3f}ms".format(s * 1000))
+        # if s < 1.0:
+        #     print("{:.3f}ms".format(s / 1000))
+        # else:
+        #     print("{:.3f}s".format(s))
---
+++ yodel/bench/grids/9x9/grids9x9-obs01.py
@@ -0,0 +1,59 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils import *
+from yodel import sites9, truth9
+
+def probfn(i, j):
+   match (i, j):
+       case (0, 0): return [0.5]
+       case (_, 0): return [1./5, 1./6]
+       case (0, _): return [1./3, 1./4]
+       case (1, _): return [1.0 / 7.0, 1.0 / 8.0, 1.0 / 9.0, 1.0 / 11.0]
+       case (2, _): return [2.0 / 7.0, 2.0 / 8.0, 2.0 / 9.0, 2.0 / 11.0]
+       case (3, _): return [6.0 / 7.0, 6.0 / 8.0, 6.0 / 9.0, 6.0 / 11.0]
+       case (4, _): return [3.0 / 7.0, 3.0 / 8.0, 8.0 / 9.0, 9.0 / 11.0]
+       case (5, _): return [3.0 / 7.0, 3.0 / 8.0, 8.0 / 9.0, 9.0 / 11.0]
+       case (6, _): return [6.0 / 7.0, 6.0 / 8.0, 6.0 / 9.0, 6.0 / 11.0]
+       case (7, _): return [3.0 / 7.0, 3.0 / 8.0, 8.0 / 9.0, 9.0 / 11.0]
+       case (8, _): return [3.0 / 7.0, 3.0 / 8.0, 8.0 / 9.0, 9.0 / 11.0]
+       case _: raise Exception()
+
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="generate data for simple HMMs")
+    parser.add_argument("--num-samples", default=10_000, type=int,)
+    parser.add_argument("--num-runs", default=10, type=int,)
+    parser.add_argument("--seed", default=0, type=int,)
+    args = parser.parse_args()
+
+    model = lambda: mkgrid(9, probfn)
+    model = pyro.condition(model, {"flip01": torch.tensor(0.0)})
+
+    if args.num_runs > 1:
+        (l1s, times) = runall(model, sites9, truth9, num_runs=args.num_runs, num_samples=args.num_samples, start_seed=args.seed)
+        print("--------")
+        runs = len(l1s)
+        print(f"averages over {runs} runs:")
+        print("wallclock:", sum(times) / len(times), "s")
+        print("       L1:", sum(l1s) / len(l1s))
+    else:
+        # we are benchmarking, expect the same output as yodel
+        torch.manual_seed(args.seed)
+        np.random.seed(args.seed)
+        random.seed(args.seed)
+        start = time.time()
+        importance = Importance(model, num_samples=args.num_samples)
+        posterior = importance.run()
+        xs = allmarg(posterior, sites9, num_samples=args.num_samples)
+        end = time.time()
+        s = end - start
+        print(" ".join([f"{x}" for x in xs]))
+        print("{:.3f}ms".format(s * 1000))
+        # if s < 1.0:
+        #     print("{:.3f}ms".format(s / 1000))
+        # else:
+        #     print("{:.3f}s".format(s))
