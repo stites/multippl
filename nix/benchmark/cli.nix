a9701e24fab603f8e9ea23424c79523f689afe0c
Sam Stites <stites@users.noreply.github.com>
2024-12-20 21:33:32 -0500

    full bench pipeline

--- nix/benchmark/cli.nix
+++ nix/benchmark/cli.nix
@@ -88,6 +88,15 @@ in
       for exp in grids/3x3 grids/6x6 grids/9x9 arrival/tree-15 arrival/tree-31 arrival/tree-63 gossip/g4 gossip/g10 gossip/g20 bayesnets/insurance bayesnets/alarm; do
           ((echo \$exp && cd $out/bench/\$exp && ${pyro}/bin/python ./bench.py "\$@" --logdir \$WORKING_DIR/logs/\$exp) || exit 1)
       done
+      echo "non-PSI evaluations complete. running PSI evaluations now."
+      sleep 3
+      for exp in grids/3x3 grids/6x6 grids/9x9 arrival/tree-15 arrival/tree-31 arrival/tree-63 gossip/g4 gossip/g10 gossip/g20 bayesnets/insurance bayesnets/alarm; do
+          ((echo \$exp && cd $out/bench/\$exp && ${pyro}/bin/python ./bench.py "\$@" --psi --logdir \$WORKING_DIR/logs/\$exp) || exit 1)
+      done
+
+      echo "all evaluations complete. tabulating results now..."
+      sleep 3
+      ((echo \$exp && cd $out/bench/ && ${pyro}/bin/python ./tabulate.py --logdir \$WORKING_DIR/logs) || exit 1)
       EOF
       chmod a+x $out/bin/multippl-benchmark
     '';
